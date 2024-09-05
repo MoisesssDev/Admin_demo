41f3763916f5509a5f62db3d66ff69d19b42526f
Moises Almeida <moisesssprg@gmail.com>
2024-09-04 21:28:47 -0300

    create authenticate from User

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
 Product.create!(name: "Produto 1", price: 19.99, description: "Descrição do Produto 1")
 Product.create!(name: "Produto 2", price: 29.99, description: "Descrição do Produto 2")
 Product.create!(name: "Produto 3", price: 39.99, description: "Descrição do Produto 3")
+
+puts "Seeds created successfully!"
